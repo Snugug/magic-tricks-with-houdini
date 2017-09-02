bf4ad9866332afefab4baee4ec80f6b9cda7172e
Sam Richard <sam@snug.ug>
2017-09-02 18:34:51 -0400

    :new: Massive content update

--- js/main.js
+++ js/main.js
@@ -17,6 +17,27 @@ editor.run('.editor', {
   live: true,
 });
 
+const borderColors = {
+  '<color>+': {
+    initial: 'currentcolor',
+    items: [
+      '--border-top-color',
+      '--border-right-color',
+      '--border-bottom-color',
+      '--border-left-color',
+    ],
+  },
+  '<number>': {
+    initial: '0',
+    items: [
+      '--border-top-width',
+      '--border-right-width',
+      '--border-bottom-width',
+      '--border-left-width',
+    ],
+  },
+};
+
 if (window.CSS) {
   window.CSS.registerProperty({
     name: '--registered-color',
@@ -24,9 +45,22 @@ if (window.CSS) {
     inherits: false,
     initialValue: 'rebeccapurple',
   });
+
+  for (type in borderColors) {
+    const item = borderColors[type];
+    item.items.forEach(prop => {
+      window.CSS.registerProperty({
+        name: prop,
+        syntax: type,
+        inherits: false,
+        initialValue: item.initial,
+      });
+    });
+  }
 }
 
 if (window.CSS.paintWorklet) {
   window.CSS.paintWorklet.addModule('js/circle.not.min.js');
   window.CSS.paintWorklet.addModule('js/face.not.min.js');
+  window.CSS.paintWorklet.addModule('js/border-colors.not.min.js');
 }
