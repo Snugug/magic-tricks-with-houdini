cb8e0a5b59525901aeda11dab0143876d3fabf57
Sam Richard <sam@snug.ug>
2018-08-24 17:33:40 -0500

    :new: Update styling to match Houdini Glitch site

--- js/main.js
+++ js/main.js
@@ -58,6 +58,48 @@ if (window.CSS) {
     initialValue: 'rebeccapurple',
   });
 
+  CSS.registerProperty({
+    name: '--brush-color',
+    syntax: '<color>',
+    inherits: true,
+    initialValue: '#784ca5',
+  });
+
+  CSS.registerProperty({
+    name: '--offset',
+    syntax: '<number>',
+    inherits: true,
+    initialValue: .25,
+  });
+
+  CSS.registerProperty({
+    name: '--padding',
+    syntax: '<number>',
+    inherits: true,
+    initialValue: 5,
+  });
+
+  CSS.registerProperty({
+    name: '--art-color',
+    syntax: '<color>',
+    inherits: false,
+    initialValue: 'white',
+  });
+
+  CSS.registerProperty({
+    name: '--art-steps',
+    syntax: '<number>',
+    inherits: false,
+    initialValue: 40,
+  });
+
+  CSS.registerProperty({
+    name: '--art-alpha',
+    syntax: '<number>',
+    inherits: false,
+    initialValue: 1,
+  });
+
   for (const type in borderColors) {
     const item = borderColors[type];
     item.items.forEach(prop => {
@@ -72,8 +114,14 @@ if (window.CSS) {
 }
 
 if (window.CSS.paintWorklet) {
-  window.CSS.paintWorklet.addModule('js/circle.not.min.js');
-  window.CSS.paintWorklet.addModule('js/face.not.min.js');
-  window.CSS.paintWorklet.addModule('js/warning.not.min.js');
-  window.CSS.paintWorklet.addModule('js/corner.not.min.js');
+  CSS.paintWorklet.addModule('js/circle.not.min.js');
+  CSS.paintWorklet.addModule('js/face.not.min.js');
+  CSS.paintWorklet.addModule('js/warning.not.min.js');
+  CSS.paintWorklet.addModule('js/corner.not.min.js');
+  CSS.paintWorklet.addModule('js/brushstroke.not.min.js');
+  CSS.paintWorklet.addModule('js/holman.not.min.js');
+}
+
+if (window.CSS.layoutWorklet) {
+  CSS.layoutWorklet.addModule('js/blueprint.not.min.js');
 }
