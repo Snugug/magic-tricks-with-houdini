3067bd4bbd63be2e3e6c518cc887fddd404b96eb
Sam Richard <sam@snug.ug>
2018-09-12 11:39:51 -0500

    :racehorse: Convert gifs to videos

--- config/default.js
+++ config/default.js
@@ -5,5 +5,6 @@ const armadillo = require('gulp-armadillo/config');
 armadillo.tasks.watch[0].push('copy:js:watch');
 armadillo.tasks.copy[0].push('copy:js');
 armadillo.tasks.build.push('clean:deploy:html');
+armadillo.sw.browser.include = false;
 
 module.exports = armadillo;
